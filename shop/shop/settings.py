68455d55e1340bc63b1e9fce4e26ab6bff0c7fd2
Marya373 <z89262554967@gmail.com>
2024-01-31 15:29:07 +0300

    Did you install mysqlclient?

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'OPTIONS':{
             'init_command': "SET NAMES 'utf8mb4'; SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
-        },
+        }
     }
 
 }
