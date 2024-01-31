71e68af26c9061c868ccc109e5dc6268fa3e7ffb
Marya373 <z89262554967@gmail.com>
2024-01-31 11:09:54 +0300

    1

---
+++ shop/shopapp/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from shopapp.models import Client, Product, Order
+
+@admin.action(description="Сбросить количество в ноль")
+def reset_quantity(modeladmin, request, queryset):
+    queryset.update(quantity=0)
+
+# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'description', 'quantity', 'image']
+    ordering=['name', 'quantity']
+    list_filter=['date_time_additions_product']
+    search_fields =['name']
+    actions = [reset_quantity]
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['client', 'date_time_placing_order']
+    list_filter=['client', 'date_time_placing_order']
+    search_fields =['client']
+
+admin.site.register(Client)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
+
+
